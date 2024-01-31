10809aed665c9f7141181f522bd285e9fe6d4774
Utpal kant <utpalkant098@gmail.com>
2024-02-01 02:00:15 +0530

    initial commit

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,74 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./footer.css";
 
 export default function Footer() {
   return (
-    <footer className="bg-white border-y">
-      <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
-        <div className="md:flex md:justify-between">
-          <div className="mb-6 md:mb-0">
-            <Link to="/" className="flex items-center">
-              <img
-                // src="https://iili.io/JcyGYsp.webp"
-                src="https://iili.io/Jl9z0a2.png"
-                //src="https://iili.io/Jl92jtV.png"
-                className="mr-3 h-16"
-                alt="Logo"
-              />
-            </Link>
+    <footer className="bg-white border-y absolute left-0 right-0 bottom-0">
+      <div className="mx-auto w-full max-w-screen-xl p-4 py-4 lg:py-6">
+        <div className="sm:flex sm:items-center sm:justify-between border border-red-300">
+          <div className="flex sm:items-center sm:justify-center border border-purple-300">
+            <img
+              src="https://iili.io/Jl9z0a2.png"
+              alt="logo"
+              className="h-10"
+            />
           </div>
-          <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
-            <div>
-              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">
-                Resources
-              </h2>
-              <ul className="text-gray-500 font-medium">
-                <li className="mb-4">
-                  <Link to="/" className="hover:underline">
-                    Home
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/about" className="hover:underline">
-                    About
-                  </Link>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">
-                Follow us
-              </h2>
-              <ul className="text-gray-500 font-medium">
-                <li className="mb-4">
-                  <a
-                    href="https://github.com/Utpalkant6204"
-                    className="hover:underline"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    Github
-                  </a>
-                </li>
-                <li>
-                  <Link to="/" className="hover:underline">
-                    LinkedIn
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
-        <div className="sm:flex sm:items-center sm:justify-between">
-          <span className="text-sm text-gray-500 sm:text-center">
-            © 2024
-            <a href="/" className="hover:underline">
-              utpalkant
-            </a>
-            . All Rights Reserved.
-          </span>
-          <div className="flex mt-4 space-x-5 sm:justify-center sm:mt-0">
+
+          <div className="flex mt-4 space-x-5 sm:justify-center footer">
             <Link to="#" className="text-gray-500 hover:text-gray-900">
               <svg
                 className="w-4 h-4"
@@ -91,11 +38,15 @@ export default function Footer() {
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="currentColor"
-                viewBox="0 0 21 16"
+                viewBox="0 0 18 18"
               >
-                <path d="M16.942 1.556a16.3 16.3 0 0 0-4.126-1.3 12.04 12.04 0 0 0-.529 1.1 15.175 15.175 0 0 0-4.573 0 11.585 11.585 0 0 0-.535-1.1 16.274 16.274 0 0 0-4.129 1.3A17.392 17.392 0 0 0 .182 13.218a15.785 15.785 0 0 0 4.963 2.521c.41-.564.773-1.16 1.084-1.785a10.63 10.63 0 0 1-1.706-.83c.143-.106.283-.217.418-.33a11.664 11.664 0 0 0 10.118 0c.137.113.277.224.418.33-.544.328-1.116.606-1.71.832a12.52 12.52 0 0 0 1.084 1.785 16.46 16.46 0 0 0 5.064-2.595 17.286 17.286 0 0 0-2.973-11.59ZM6.678 10.813a1.941 1.941 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.919 1.919 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Zm6.644 0a1.94 1.94 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.918 1.918 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Z" />
+                <path
+                  fillRule="evenodd"
+                  d="M1.84 0A1.84 1.84 0 0 0 0 1.84V16.16A1.84 1.84 0 0 0 1.84 18H16.16A1.84 1.84 0 0 0 18 16.16V1.84A1.84 1.84 0 0 0 16.16 0H1.84zM4.6 15H2V6h2.6v9zM3.3 5.44a2.29 2.29 0 0 0-2.29-2.29A2.29 2.29 0 0 0 0 5.44a2.29 2.29 0 0 0 2.29 2.29A2.29 2.29 0 0 0 3.3 5.44zM15 15h-2.6V9.48c0-2.26-2.69-2.08-2.69 0V15H7V6h2.6v1.19c1.1-2.05 5.4-2.19 5.4 2.46z"
+                  clipRule="evenodd"
+                />
               </svg>
-              <span className="sr-only">Discord community</span>
+              <span className="sr-only">LinkedIn page</span>
             </Link>
             <Link to="#" className="text-gray-500">
               <svg
@@ -145,6 +96,13 @@ export default function Footer() {
               </svg>
               <span className="sr-only">Dribbble account</span>
             </Link>
+          </div>
+          <div className="text-sm text-gray-500 sm:text-center sm:mt-2">
+            © 2024
+            <a href="/" className="hover:underline">
+              utpalkant
+            </a>
+            . All Rights Reserved.
           </div>
         </div>
       </div>
