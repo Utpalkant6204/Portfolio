65b81a78296f41e344f22f7045851379acfaa697
Utpal kant <utpalkant098@gmail.com>
2024-03-03 22:50:39 +0530

    adding heading about

--- src/Components/Education.js
+++ src/Components/Education.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Education = () => {
   return (
-    <div className="my-14 mx-24 max-[600px]:mx-4 max-[1000px]:mx-8">
+    <div className="my-14 mx-4 sm:mx-8 md:mx-16 lg:mx-24 xl:mx-32">
       <h2
         style={{
           fontFamily: "GT Pressura Mono, monospace",
@@ -12,12 +12,12 @@ const Education = () => {
           color: "#F1F1F1",
           fontWeight: "bold",
         }}
-        className="pt-10 pb-8 px-10 max-[600px]:px-5"
+        className="pt-10 pb-8 px-10 sm:px-8 md:px-6 lg:px-4 xl:px-2"
       >
         Education
       </h2>
-      <div className="px-10 mx-[750]:px-5">
-        <div className="flex justify-between">
+      <div className="px-10 mx-4 sm:mx-8 md:mx-12 lg:mx-16 xl:mx-20">
+        <div className="flex flex-col sm:flex-row justify-between">
           <h3
             style={{
               fontFamily: "GT Pressura",
@@ -27,7 +27,11 @@ const Education = () => {
             }}
           >
             <span className="me-5">🎓</span>
-            <a href="https://iiitranchi.ac.in/" target="_blank">
+            <a
+              href="https://iiitranchi.ac.in/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               Indian Institute of Information Technology
             </a>
           </h3>
@@ -48,7 +52,7 @@ const Education = () => {
             color: "#F1F1F1",
             fontSize: "18px",
           }}
-          className="mx-10 px-3"
+          className="mx-4 sm:mx-8 md:mx-12 lg:mx-16 xl:mx-20 px-3"
         >
           B. Tech (Hons) in Computer Science and Engineering
         </p>
